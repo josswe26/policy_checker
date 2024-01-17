6441d892e64688b6aaa5fc4b865c9a7109053e72
Jose <jsanchez026@gmail.com>
2024-01-18 00:37:05 +0100

    Add API endpoint for policy checks

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
+from policy_manager import PolicyManager
+from credit_policy import IncomeCheck, DebtCheck, PaymentRemarksCheck, AgeCheck
 
 app = Flask(__name__)
 
@@ -7,6 +10,25 @@ app = Flask(__name__)
 def index():
     """Index page for the API"""
     return "Policy Checker API"
+
+
+@app.route("/api/v1/policy_check", methods=["POST"])
+def check_policy():
+    """API endpoint to check the policies"""
+
+    try:
+        data = request.get_json()
+    except BadRequest:
+        return jsonify({"error": "INVALID_JSON"})
+
+    policy_manager = PolicyManager()
+
+    policy_manager.add_policy(IncomeCheck())
+    policy_manager.add_policy(DebtCheck())
+    policy_manager.add_policy(PaymentRemarksCheck())
+    policy_manager.add_policy(AgeCheck())
+
+    return policy_manager.check_all_policies(data)
 
 
 if __name__ == "__main__":
